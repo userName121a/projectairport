bb8b094b99957bfde09be42aa130723eba1fcbc9
userName121a <januszk912@gmail.com>
2017-05-27 15:38:57 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt2
+{
+    class Client
+    {
+
+    }
+    class IndividualClient : Client
+    {
+
+    }
+    class CompanyClient : Client
+    {
+
+    }
+}
