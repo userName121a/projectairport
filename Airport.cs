65038b84f63c9c64e65eb6a25a0d1a1635ef01dc
userName121a <januszk912@gmail.com>
2017-05-29 20:54:25 +0200

    Update Airport.cs

--- Airport.cs
+++ Airport.cs
@@ -9,15 +9,35 @@ namespace projekt2
     class Airport
     {
         Airplane plane;
-        coord width;
-        coord length;
+        int width;
+        int length;
         String PlaceName;
-        class coord
+        /*class coord
         {
             int deg;
-            int min;
-            int sec;
+            //int min;
+            //int sec;
+            coord () = delete;
+         
+            coord(int d, int m=0, int s=0)
+            {
+                deg = d;
+                //min = m;
+                //sec = s;
+            }
+            
+        }*/
+        Airport(int w,int l)
+        {
+            width = w;
+            length = l;
         }
+        int distance(Airport a, Airport b)
+        {
+            int dist = sqrt( (a.w-b.w)(a.w-b.w) + (a.l-b.l)(a.l-b.l) );
+            return dist;
+        }
+        
      
     }
 }
