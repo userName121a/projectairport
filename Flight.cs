ee57406f291cdd9e33d943719184b903c05fa23d
userName121a <januszk912@gmail.com>
2017-06-04 16:20:36 +0200

    Update Flight.cs

--- Flight.cs
+++ Flight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,16 @@ namespace projekt2
 {
     class Flight
     {
+        Track track;
+        DateTime begintime;
+        DateTime endtime;
+        Airplane airplane;
+        Flight(ref Track track, DateTime begin,ref Airplane airplane)
+        {
+            this.track = track;
+            begintime = begin;
+            endtime = begin + this.track.getTimeLength();
+            this.airplane = airplane;
+        }
     }
 }
