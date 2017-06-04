164f9dbdb6d151fdd665859e89b512697b0644e1
userName121a <januszk912@gmail.com>
2017-06-04 16:21:09 +0200

    Update Airport.cs

--- Airport.cs
+++ Airport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,37 @@ namespace projekt2
 {
     class Airport
     {
+        private String name;
+        private int width;
+        public int height;
+        private int id;
+        
+        private static int counter = 0;
+        public Airport(int width,int height,String name)
+        {
+            this.name = name;
+            this.width = width;
+            this.height = height;
+            id = ++counter;
+        }
+        public Airport(Airport airport)
+        {
+            name = airport.name;
+            width = airport.width;
+            height = airport.height;
+            id = ++counter;
+        }
+
+        public int getWidth() { return width; }
+        public int getHeigth() { return height; }
+        public String getName() { return name; }
+
+        public override String ToString()
+        {
+            return name + " " + id + "\nwidth " + width + "\nheight " + height + "\n";
+        }
+
+
+
     }
 }
