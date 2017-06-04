358c896aaae25043333b069cea459f9a763a3b4c
userName121a <januszk912@gmail.com>
2017-06-04 16:21:48 +0200

    Update Airplane.cs

--- Airplane.cs
+++ Airplane.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,35 @@ namespace projekt2
 {
     class Airplane
     {
-        int places;
-        int range;
+        static private int counter = 0;
+        private int places;
+        private int range;
+        private int id;
+
+        public Airplane(int places, int range)
+        {
+            this.places = places;
+            this.range = range;
+            id = ++counter;
+
+        }
+        public Airplane(Airplane airplane)
+        {
+            places = airplane.places;
+            range = airplane.range;
+            id = ++counter;
+        }
+        public int getPlaces() { return places; }
+        public int getRange() { return range; }
+        public int getId() { return id; }
+        public static int getPlanesCount() { return counter; }
         
+        override public String ToString()
+        {
+            String propert = "Plane nr  "+ id + "\nplaces " + places + "\nrange " + range + "\n";
+            return propert;
+        }
         
+
     }
 }
